56478d51f95201bf95c8d8c521b3e2295243baad
BQ Lab <beaqon@naver.com>
2019-03-16 23:09:30 +0900

    v2.4

--- app/src/main/java/app/bqlab/mediandbacks/MainActivity.java
+++ app/src/main/java/app/bqlab/mediandbacks/MainActivity.java
@@ -131,6 +131,7 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                            mBluetooth.getSetting().disconnect();
                             finish();
                         }
                     }).show();
